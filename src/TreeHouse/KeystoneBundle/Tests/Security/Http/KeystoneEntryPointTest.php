76852094850e8af6686f81c220a8ed3d71e137eb
Mark van Duijker <mark@duyker.nl>
2015-12-08 16:41:32 +0100

    Added test with AuthenticationException

--- src/TreeHouse/KeystoneBundle/Tests/Security/Http/KeystoneEntryPointTest.php
+++ src/TreeHouse/KeystoneBundle/Tests/Security/Http/KeystoneEntryPointTest.php
@@ -4,6 +4,7 @@ namespace TreeHouse\KeystoneBundle\Tests\Security\Http;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use TreeHouse\KeystoneBundle\Security\Http\KeystoneEntryPoint;
 
@@ -12,7 +13,20 @@ class KeystoneEntryPointTest extends \PHPUnit_Framework_TestCase
     public function testStart()
     {
         $request = $this->prophesize(Request::class);
+        $entryPoint = new KeystoneEntryPoint();
+        $authException = new AuthenticationException('The exception message');;
 
+        $this->assertInstanceOf(AuthenticationEntryPointInterface::class, $entryPoint);
+
+        $response = $entryPoint->start($request->reveal(), $authException);
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(401, $response->getStatusCode());
+    }
+
+    public function testStartWithoutException()
+    {
+        $request = $this->prophesize(Request::class);
         $entryPoint = new KeystoneEntryPoint();
 
         $this->assertInstanceOf(AuthenticationEntryPointInterface::class, $entryPoint);
